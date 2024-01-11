5f3d860aae0ba1c1de2b5240f8903982c37ba90e
drewsix9 <145753941+drewsix9@users.noreply.github.com>
2024-01-11 19:56:01 +0800

    Add isPrimeTimes function to check if a number is prime efficiently

--- review.cpp
+++ review.cpp
@@ -29,11 +29,21 @@ bool isPrime(int num) {
   }
   return true;
 }
+bool isPrimeTimes(int num) {
+  if (num <= 1)
+    return false;
+
+  for (int i = 2; i * i <= (num); i++) {
+    if (num % i == 0)
+      return false;
+  }
+  return true;
+}
 
 int main() {
   auto timeStart = clock();
   for (int i = 0; i < 100000; i++) {
-    if (isPrime(i)) {
+    if (isPrimeTimes(i)) {
       cout << i << endl;
     }
   }
