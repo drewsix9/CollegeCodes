c105b35fa62b26690a440a6875604331659b53a6
drewsix9 <danmathewgamale@gmail.com>
2024-02-22 22:41:58 +0800

    logged in to yernotgud

--- Tutorial/problem4.c
+++ Tutorial/problem4.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 
-int main()
-{
-    int size;
-    printf("Input the sisze of array: ");
-    scanf("%d", &size);
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        printf("element - %d : ", i);
-        scanf("%d", &arr[i]);
-    }
+int main() {
+  int size;
+  printf("Input the sisze of array: ");
+  scanf("%d", &size);
+  int arr[size];
+  for (int i = 0; i < size; i++) {
+    printf("element - %d : ", i);
+    scanf("%d", &arr[i]);
+  }
 
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] % 2 == 0)
-        {
-            arr[i] = 0;
-        }
-        else
-        {
-            arr[i] = 1;
-        }
+  for (int i = 0; i < size; i++) {
+    if (arr[i] % 2 == 0) {
+      arr[i] = 0;
+    } else {
+      arr[i] = 1;
     }
+  }
 
-    printf("\nElements of array after replacement:\n");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+  printf("\nElements of array after replacement:\n");
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
 
-    return 0;
+  return 0;
 }
