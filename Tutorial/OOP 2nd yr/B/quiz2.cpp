5c3bf1e6353449fec92316ae11544652dc7cd68f
drewsix9 <danmathewgamale@gmail.com>
2024-02-29 20:32:17 +0800

    Add function to check for consecutive digits

--- Tutorial/OOP 2nd yr/B/quiz2.cpp
+++ Tutorial/OOP 2nd yr/B/quiz2.cpp
@@ -3,44 +3,45 @@
 
 using namespace std;
 
-bool isPrime(int n) {
-  if (n <= 1)
-    return false;
-  for (int i = 2; i < n; i++) {
-    if (n % i == 0) {
-      return false;
-    }
-  }
-  return true;
+char incrementChar(char c) {
+  // Convert the character 'c' to its corresponding integer value by subtracting the ASCII value of '0'
+  int intValue = c - '0';
+
+  // Increment the integer value by 1
+  intValue = intValue + 1;
+
+  // Convert the incremented integer value back to its corresponding character by adding the ASCII value of '0'
+  char result = intValue + '0';
+
+  // Return the incremented character
+  return result;
+
+  // The above code can be written in a single line as follows:
+  // return (char)((c - '0') + 1 + '0');
 }
 
-int addDigits(int num) {
-  int sum = 0;
-  while (num != 0) {
-    sum += num % 10;
-    num /= 10;
+bool hasConsecutiveDigits(char *token) {
+  int len = strlen(token);
+  // len - 1 because we want to end right before the last character
+  for (int i = 0; i < len - 1; i++) {
+    if (token[i + 1] == incrementChar(token[i])) {
+      return true;
+    }
   }
-  return sum;
+  return false;
 }
 
 int main() {
-  int arr[100];
-  int num = 1;
-  int n = 0;
+  char str[100];
   cout << "Enter inputs: ";
-  while (1) {
-    cin >> num;
-    if (num == 0) {
-      break;
-    }
-    arr[n] = num;
-    n++;
-  }
+  cin.getline(str, 100);
 
   cout << "\nOutput: ";
-  for (int i = 0; i < n; i++) {
-    if (!isPrime(addDigits(arr[i]))) {
-      cout << arr[i] << " ";
+  char *token = strtok(str, " ");
+  while (token != NULL) {
+    if (!hasConsecutiveDigits(token)) {
+      cout << token << " ";
     }
+    token = strtok(NULL, " ");
   }
 }
