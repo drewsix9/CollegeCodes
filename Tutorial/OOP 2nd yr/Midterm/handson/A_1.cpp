622b70b1d464f9c60359b49cb0cc08545ff1a978
drewsix9 <danmathewgamale@gmail.com>
2024-03-20 19:25:08 +0800

    Refactor word deletion logic in A_1.cpp

--- Tutorial/OOP 2nd yr/Midterm/handson/A_1.cpp
+++ Tutorial/OOP 2nd yr/Midterm/handson/A_1.cpp
@@ -20,17 +20,15 @@ int main() {
   cout << "Enter a word to delete: ";
   cin.getline(word, 100);
 
-  char *ptr = str;                               // Declare a pointer to the character array 'str'
-  int i = 0;                                     // Initialize a counter variable
-  cout << "\nOutput: ";                          // Print a message to the console
-  while (i < strlen(str)) {                      // Loop through the characters in 'str'
-    if (strncmp(ptr, word, strlen(word)) == 0) { // If the current sequence of characters in 'str' matches 'word'
-      ptr += strlen(word);                       // Move the pointer forward by the length of 'word'
-      i += strlen(word);                         // Increment the counter by the length of 'word'
-    } else {                                     // If the current sequence of characters in 'str' does not match 'word'
-      cout << *ptr;                              // Print the current character to the console
-      ptr++;                                     // Move the pointer forward by one
-      i++;                                       // Increment the counter by one
+  char *ptr = str;
+  int i = 0;
+  cout << "\nOutput: ";
+  while (i < strlen(str)) {
+    if (strncmp(ptr + i, word, strlen(word)) == 0) {
+      i += strlen(word);
+    } else {
+      cout << *(ptr + i);
+      i++;
     }
   }
 }
