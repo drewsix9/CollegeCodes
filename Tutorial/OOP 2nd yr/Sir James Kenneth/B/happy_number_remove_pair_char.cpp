89e413f24f87451f68c831a52d7270b83b14cdc2
drewsix9 <danmathewgamale@gmail.com>
2024-04-25 21:22:30 +0800

    Refactor reverseStr, removePairChars, and sortStr functions in happy_number_remove_pair_char.cpp

--- Tutorial/OOP 2nd yr/Sir James Kenneth/B/happy_number_remove_pair_char.cpp
+++ Tutorial/OOP 2nd yr/Sir James Kenneth/B/happy_number_remove_pair_char.cpp
@@ -3,18 +3,25 @@
 
 using namespace std;
 
+// Function to reverse a string
 string reverseStr(string &str) {
   int size = str.length();
+  // Swap characters from start and end
   for (int i = 0; i < size / 2; i++) {
-    swap(str[i], str[size - i - 1]);
+    int temp = str[i];
+    str[i] = str[size - i - 1];
+    str[size - i - 1] = temp;
   }
   return str;
 }
 
+// Function to remove pair of characters from a string
 string removePairChars(string &str) {
   int size = str.length();
+  // Iterate through the string
   for (int i = 0; i < size; i++) {
     for (int j = i + 1; j < size; j++) {
+      // If pair found, remove both characters
       if (str[i] == str[j]) {
         str.erase(j, 1);
         str.erase(i, 1);
@@ -27,41 +34,50 @@ string removePairChars(string &str) {
   return str;
 }
 
+// Function to sort a string
 string sortStr(string &str) {
   int size = str.length();
+  // Bubble sort algorithm
   for (int i = 0; i < size; i++) {
     for (int j = i + 1; j < size; j++) {
+      // Swap if current character is greater than next
       if (str[i] > str[j]) {
-        swap(str[i], str[j]);
+        int temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
       }
     }
   }
   return str;
 }
 
+// Class to represent a number
 class Number {
 public:
   int num;
-  Number(int x) : num(x) {}
+  Number(int x) : num(x) {} // Constructor to initialize the number
 };
 
+// Class to check if a number is happy or not
 class HappyNumber : public Number {
 public:
-  HappyNumber(int num) : Number(num) {}
+  HappyNumber(int num) : Number(num) {} // Constructor to initialize the number
   bool isHappy() {
     int sum = 0;
+    // Calculate the sum of squares of digits
     while (num > 0) {
       int digit = num % 10;
       sum += digit * digit;
       num /= 10;
     }
+    // If sum is 1, then number is happy
     if (sum == 1) {
       return true;
-    } else if (sum == 4) {
+    } else if (sum == 4) { // If sum is 4, then number is not happy
       return false;
     } else {
       num = sum;
-      return isHappy();
+      return isHappy(); // Recursively check if new number is happy
     }
   }
 };
@@ -71,12 +87,15 @@ int main() {
   cout << "Enter input: ";
   getline(cin, input);
   HappyNumber hn = HappyNumber(input.length());
+  // Check if length of input is a happy number
   if (hn.isHappy()) {
     string sorted = sortStr(input);
     cout << "input length is HAPPY number" << endl;
+    // If happy, sort the string and remove pair of characters
     cout << "Output:" << removePairChars(sorted) << endl;
   } else {
     cout << "input length is UNHAPPY number" << endl;
+    // If not happy, reverse the string
     cout << "Output:" << reverseStr(input) << endl;
   }
 }
