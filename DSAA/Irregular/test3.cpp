3970cd835944186a0ed48379fd4b5485d46fdc57
Dan Mathew Gamale <danmathewgamale@gmail.com>
2025-01-07 09:39:49 +0800

    irreg finals

--- DSAA/Irregular/test3.cpp
+++ DSAA/Irregular/test3.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
+
 using namespace std;
 
-
-int hasConsecutive(string &str) {
-	for (size_t i = 0; i < str.size() - 1; i++) {
-		if (str[i] == str[i + 1])
-			return i;
-	}
-	return -1;
-}
-
-void deleteConsecutive(string &str, int i) {
-	if (i == -1) {
-		return;
-	}
-	str.erase(i, 2);
-	return;
-}
-
-void process(string &str) {
-	int i = hasConsecutive(str);
-	while (i != -1) {
-		deleteConsecutive(str, i);
-		i = hasConsecutive(str);
-	}
+void printPrimeFactors(int num) {
+  int i = 2;
+  while (num > 1) {
+    if (num % i == 0) {
+      cout << i << " ";
+      num /= i;
+    } else {
+      i++;
+    }
+  }
 }
 
 int main() {
-	string line, word;
-	vector<string> input;
-	cout << "Enter input:\n";
-	while (line.back() != '.') {
-		getline(cin, line);
-		input.push_back(line);
-	}
-	cout << "\nOutput:\n";
-	for (string s : input) {
-		stringstream ss(s);
-		while (ss >> word) {
-			process(word);
-			cout << word << " ";
-		}
-		cout << endl;
-	}
+  int num = 0;
+  cout << "Enter a number: ";
+  cin >> num;
+  printPrimeFactors(num);
+  return 0;
 }
